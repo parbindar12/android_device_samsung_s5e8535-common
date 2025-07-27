a865bd68ad9d9cb19c97bbe4b6a06ceb48a58b53
Bruno Martins <bgcngm@gmail.com>
2025-07-27 20:26:53 +0530

    s5e8535-common: Migrate to AIDL touch hal
    Change-Id: Ie0c2dd615652e80032393b04112fc39803205814

--- common.mk
+++ common.mk
@@ -272,7 +272,7 @@ PRODUCT_COPY_FILES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung
+    vendor.lineage.touch-service.samsung
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
